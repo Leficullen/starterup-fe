c4acf1a9de7a30d06dfe44f53b2156ee5b07552a
Leficullen <lefirizqy2007@gmail.com>
2025-11-18 23:02:15 +0700

    fix: adjust navbar icon on mobile screen

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,19 +6,19 @@ import { Home, HomeIcon } from "lucide-react";
 const Navbar = () => {
   return (
     <div className="fixed bg-background/20 py-[1.3%] top-0 w-full px-[3%] backdrop-blur-sm border-b shadow-drop shadow-md flex justify-between items-center z-100 h-[70px]">
-      <div className="hidden md:block">
+      <div className="">
         <Link
           href="/"
-          className="font-bold text-2xl text-background bg-linear-to-r from-primary to-accent px-3 py-1 rounded-full text-shadow-2xl"
+          className="font-bold md:text-2xl text-background bg-linear-to-r from-primary to-accent px-3 py-3 md:py-1 rounded-full text-shadow-2xl"
         >
           Shrimp Linkers
         </Link>
       </div>
-      <div className="flex md:hidden">
+      {/* <div className="flex md:hidden">
         <Link href="/">
           <HomeIcon width="30px" height="30px" className="text-primary" />
         </Link>
-      </div>
+      </div> */}
       <div className="">
         <Link href="/login">
           <Button className="font-semibold text-sm md:text-lg"> Login </Button>
